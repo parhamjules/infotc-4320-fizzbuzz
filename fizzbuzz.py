35d7d3facace0d6b4792070d1bda9f4d27dbfab9
Jules Parham <parhamjules@pop-os.localdomain>
2022-10-09 19:48:45 -0500

    program iteration 2 completed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,11 @@
-# FizzBuzz Challenge - Iteration 1
-# Task: Get the program to work.
+# FizzBuzz Challenge - Iteration 2
+# Task: Improve efficiency of the program.
 
 for n in range(1, 101):
-    if n % 3 == 0 and n % 5 == 0:
-        print('FizBuzz')
-    elif n % 3 == 0:
-        print('Fizz')
-    elif n % 5 == 0:
-        print('Buzz')
-    else:
+    if n % 3 != 0 and n % 5 != 0:
         print(n)
+        continue
+    res = ""
+    if n % 3 == 0: res += "Fizz"
+    if n % 5 == 0: res += "Buzz"
+    print(res)
